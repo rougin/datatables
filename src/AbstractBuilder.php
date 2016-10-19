3d05454f4b5ccef5ffcd2eef62f44cecf6eebd12
Rougin Gutib <rougingutib@gmail.com>
2016-10-20 02:54:07 +0800

    Improve code quality

--- src/AbstractBuilder.php
+++ src/AbstractBuilder.php
@@ -11,6 +11,28 @@ namespace Rougin\Datatables;
 abstract class AbstractBuilder
 {
     /**
+     * Returns the data in DataTables' response format.
+     *
+     * @param  array   $data
+     * @param  integer $count
+     * @param  array   $get
+     * @return array
+     */
+    protected function getResponse(array $data, $count, array $get)
+    {
+        $search = $get['search']['value'];
+
+        $response = [];
+
+        $response['draw']            = $get['draw'];
+        $response['recordsFiltered'] = (empty($search)) ? $count : count($data);
+        $response['recordsTotal']    = $count;
+        $response['data']            = $data;
+
+        return $response;
+    }
+
+    /**
      * Removes the keys in the array.
      *
      * @param  array   $data
